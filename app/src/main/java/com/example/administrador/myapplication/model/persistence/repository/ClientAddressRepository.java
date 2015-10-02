e9e4b7f0d32ca596e118ba5c8c5323a39b5df643
Luiz Carlos <luizvaldeco@gmail.com>
2015-10-02 17:21:45 -0300

    organizando o repositorio

--- app/src/main/java/com/example/administrador/myapplication/model/persistence/repository/ClientAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.administrador.myapplication.model.persistence.repository;
-
-import com.example.administrador.myapplication.model.entities.ClientAddress;
-
-public class ClientAddressRepository {
-    public void save(ClientAddress address) {
-
-    }
-}
--- app/src/main/java/com/example/administrador/myapplication/util/AppUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.administrador.myapplication.util;
-
-import android.content.Context;
-
-public class AppUtil {
-
-    public static Context CONTEXT;
-
-}
